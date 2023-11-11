462e1b99637b4efa83d669c34f0812c5ca20c63b
Samuel <sassani134@gmail.com>
2023-11-11 20:19:04 +0100

    Adding all options possible

--- main.rb
+++ main.rb
@@ -36,7 +36,7 @@ if ARGV.length == 0 or ARGV[0] == "help" or ARGV[0] == "-h"
   exit
 end
 
-if ARGV.[0] == "keygen"
+if ARGV[0] == "keygen"
   puts "generer une paire de clé dans un fichier monRSA.pub et monRSA.priv"
   exit
   if ARGV.length == 2 and ARGV[1] == "-f"
@@ -49,7 +49,7 @@ if ARGV[0] == "crypt"
   puts "crypt a besoin d'une cle".colorize(:red)
   exit
 
-  if ARGV.length == 2 and ARGV[1] == "-c"
+  if ARGV.length == 2 and ARdGV[1] == "-c"
     puts "crypt a besoin d'une cle".colorize(:red)
     exit
   end
@@ -74,83 +74,3 @@ if ARGV[0] == "crypt"
     exit
   end
 end
-
-
-=begin
-if ARGV.length == 1
-  if ARGV[0] == "keygen"
-    p "generer une paire de clé dans un fichier monRSA.pub et monRSA.priv"
-  end
-  if ARGV[0] == "crypt"
-    puts "crypt a besoin d'une cle".colorize(:red)
-  end
-  if ARGV[0] == "decrypt"
-    puts "decrypt a besoin d'une cle privee".colorize(:red)
-  end
-else
-  puts "mauvaise syntaxe -h pour l'aide".colorize(:red)
-end
-
-if ARGV.length == 2
-  if ARGV[0] == "crypt"
-    puts "crypt a besoin d'une cle".colorize(:red)
-  end
-  if ARGV[0] == "decrypt"
-    puts "decrypt a besoin d'une cle privee".colorize(:red)
-  end
-end
-=end
-=begin
-ruby main.rb
-ruby main.rb keygen # generer une paire de clé dans un fichier monRSA.pub et monRSA.priv
-ruby main.rb crypt #X clé publique # X message ou Fichier
-ruby main.rb decrypt # X clé privée # X message ou Fichier
-ruby main.rb help
-ruby main.rb -h
-
-ruby main.rb keygen -f nomClé # generer une paire de clé dans un fichier nomClé.pub et nomClé.priv
-
-ruby main.rb crypt -c fichierCléPublique # crypter un message avec la clé publique dans un fichier par défaut messageCrypté
-ruby main.rb decrypt -c fichierCléPrivé # decrypter un message avec la clé privée dans un fichier par défaut messageDecrypté
-
-ruby main.rb crypt -c fichierCléPublique -f fichierMessageCrypté
-# crypter un message avec la clé publique dans un fichier fichierMessageCrypté.txt
-ruby main.rb decrypt -c fichierCléPrivé -f  fichierMessageDecrypté
-# decrypter un message avec la clé privée dans un fichier fichierMessageDecrypté.txt
-
-ruby main.rb crypt -c fichierCléPublique -t "message a crypter"
-# crypter un message avec la clé publique dans un fichier pardefault.txt
-ruby main.rb decrypt -c fichierCléPrivé -t "message a decrypter"
-# decrypter un message avec la clé privée dans un fichier pardefault.txt
-
-ruby main.rb crypt -c fichierCléPublique -t "message a crypter" -f fichierMessageCrypté
-# crypter un message avec la clé publique dans un fichier fichierMessageCrypté.txt
-ruby main.rb decrypt -c fichierCléPrivé -t "message a decrypter" -f  fichierMessageDecrypté
-# decrypter un message avec la clé privée dans un fichier fichierMessageDecrypté.txt
-
-=end
-
-
-# algorithme d'euclide coefficient de Bézout
-# modulo d'un entier
-#petit theoreme de fermat
-
-=begin
-Bruno veux envoyer un msg a Alice
-1. Alice clé pub et priv
-2.Bruno utilise clé pub d'alice pour crypter son message
-3.Alice recoit le message crypté
-=end
-
-=begin
-p=5
-q=17
-n = 85
-phi = 64
-e = prime & e pgcd(e,phi) = 5
-
-d*e
-
- d = e.modInverse(phi))
-
-=end
