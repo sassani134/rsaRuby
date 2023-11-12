3f3a20c340dc3feb3ed81aca3e4038b711d9ee60
Samuel <sassani134@gmail.com>
2023-11-12 19:50:54 +0100

    it looks like it works

--- prog/chiffrement.rb
+++ prog/chiffrement.rb
@@ -1,33 +1,49 @@
+require 'base64'
+
+CHEMIN_CT = "./cText/"
+
 messageClaire ="abcd" # a renseigner
-messageAscii = messageClaire.bytes
-asciiUni = Array.new
-messageChiffre = nil
+@messageAscii = messageClaire.bytes
+# @asciiUni = Array.new
+@messageChiffre = nil
 
 ayo = 97
 m = 010
 
-# n d
-publicKey = [85, 5] # a renseigner
-privateKey = 13 # a renseigner
+# n et e sont les clés publiques
+@publicKey = [85, 5] # a renseigner
+# d
+@privateKey = 13 # a renseigner
 
+# p "messageAscii : #{@messageAscii}"
+# @messageAscii.each do |n|
+#   @asciiUni << format('%03d', n)
+# end
 
-messageAscii.each do |n|
- asciiUni << format('%03d', n)
-end
-
+# p "asciiUni : #{@asciiUni}"
+# p "asciiUni : #{@asciiUni[0]}"
+# p @asciiUni[0]
+# p @asciiUni[0].class
+# p @asciiUni[0].to_i
 
 # x ≡ messageAscii^e mod n
+# (n.pow(publicKey[1])) % publicKey[0]
 
-
-
-def chiffrement(messageAscii, publicKey)
-  messageAscii.each do |n|
-    messageChiffre = (n.pow(publicKey[1])) % publicKey[0]
-    p messageChiffre
+def chiffrement(publicKey = @publicKey)
+  completedMessage = Array.new
+  @messageAscii.each do |n|
+    @messageChiffre = (n.pow(publicKey[1])) % publicKey[0]
+    @messageChiffre = format('%03d', @messageChiffre)
+    @messageChiffre = Base64.encode64(@messageChiffre)
+    @messageChiffre = @messageChiffre.delete("\n")
+    completedMessage << @messageChiffre
   end
+  # p "completedMessage : #{completedMessage}"
+  return completedMessage
 end
 
 def chiffrementDansTxt
-  file= File.open("messageChiffre", "w")
+  file = File.open("messageChiffre", "w")
 end
-chiffrement(messageAscii, publicKey)
+
+chiffrement
